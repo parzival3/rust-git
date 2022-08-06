19f524f1c1e40afd051457f2e58a4c8870be1f1f
Enrico Tolotto <etolotto@gmail.com>
2022-08-06 10:46:24 +0200

    src/main: add usage and create .git directory

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use std::process;
+
+fn print_usage() {
+    println!("usage: codecrafters-git-rust <command>");
+    println!("Available commands: ");
+    println!("\t [init]: initialize git repository");
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        if args[1] == "init" {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+            println!("Initialized git directory")
+        } else {
+            println!("unknown command: {}", args[1]);
+            print_usage();
+            process::exit(-1);
+        }
+    } else {
+        print_usage();
+        process::exit(-1);
+    }
 }
